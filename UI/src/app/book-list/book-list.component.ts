d29445b0b7664f0b3776c2b13a63ebe8f0775d7b
Ursatii <zabybenskiy@gmail.com>
2018-08-19 13:24:41 +0300

    Added Book component to UI.

---
+++ UI/src/app/book-list/book-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {BookService} from "../shared";
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  providers: [BookService]
+})
+export class BookListComponent implements OnInit {
+  books: Array<any>;
+
+  constructor(private bookService: BookService) { }
+
+  ngOnInit() {
+    this.bookService.getAll().subscribe(
+      data => {
+        this.books = data;
+      },
+      error => console.log(error)
+    )
+  }
+
+}
